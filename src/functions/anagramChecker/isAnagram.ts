fb708ad5c7a6553dbd8f05b899fa00fd3e380027
Jake Walker <jakewalk93@hotmail.com>
2022-01-09 21:23:01 +0000

    Added rule for eslinting ignore requires. changed lets to consts

--- src/functions/anagramChecker/isAnagram.ts
+++ src/functions/anagramChecker/isAnagram.ts
@@ -1,14 +1,14 @@
 export function isAnagram(string1, string2) {
-    let len1 = string1.length;
-    let len2 = string2.length;
+    const len1 = string1.length;
+    const len2 = string2.length;
 
     if (len1 !== len2) {
         console.log('Invalid Input');
         return false
     }
 
-    let str1 = string1.split('').sort().join('');
-    let str2 = string2.split('').sort().join('');
+    const str1 = string1.split('').sort().join('');
+    const str2 = string2.split('').sort().join('');
 
     if (str1 === str2) {
         console.log(`${string1} and ${string2} are anagrams`)
--- tests/functions/anagramChecker/isAnagram.test.ts
+++ tests/functions/anagramChecker/isAnagram.test.ts
@@ -2,8 +2,8 @@ import { isAnagram } from '../../../src/functions/anagramChecker/isAnagram'
 
 describe('anagram checker test suite', () => {
     it('tests isAnagram returns true when 2 strings are anagrams', () => {
-        let string1 = 'jake'
-        let string2 = 'jake'
+        const string1 = 'jake'
+        const string2 = 'jake'
 
         const result = isAnagram(string1, string2)
 
@@ -11,8 +11,8 @@ describe('anagram checker test suite', () => {
     });
 
     it('tests isAnagram returns false when 2 strings are not anagrams', () => {
-        let string1 = 'deal'
-        let string2 = 'joke'
+        const string1 = 'deal'
+        const string2 = 'joke'
 
         const result = isAnagram(string1, string2)
 
@@ -20,8 +20,8 @@ describe('anagram checker test suite', () => {
     });
 
     it('tests isAnagram returns false when 2 strings are not equal length', () => {
-        let string1 = 'deal'
-        let string2 = 'joking'
+        const string1 = 'deal'
+        const string2 = 'joking'
 
         const result = isAnagram(string1, string2)
 
